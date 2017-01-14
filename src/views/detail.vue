73b9e6c55e4d3abf068f82ef4683185d5d0f03b4
日月为易 <zhang940823@icloud.com>
2017-01-14 16:09:25 +0800

    新增商品详情页

--- src/views/detail.vue
+++ src/views/detail.vue
@@ -1,14 +1,35 @@
 <template>
     <div>
         {{msg}}
+        <com-footer @submit="submitOrder" @showCart="show = true">
+           <p slot="submit-order-slot">提交订单</p>
+        </com-footer>
+        <action-sheet :show="show" :dataSource="dataSource" @hide="show = false"></action-sheet>
     </div>
 </template>
 <script>
+    import comFooter from '../components/comFooter'
+    import actionSheet from '../components/actionSheet'
     export default {
         data(){
             return {
-                msg: "detail page"
+                msg: "detail page",
+                show: false,
             }
+        },
+         computed: {
+            dataSource(){
+               return this.$store.getters.getProducts
+            }
+        },
+        methods: {
+             submitOrder(){
+                alert("submit order");
+            },
+        },
+        components: {
+            comFooter,
+            actionSheet
         }
     }
 </script>
