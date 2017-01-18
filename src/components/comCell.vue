a16b7bdea0eeeb4a9bd4615a06ae04ec63b3086c
日月为易 <zhang940823@icloud.com>
2017-01-18 11:02:20 +0800

    修改下单页代码

---
+++ src/components/comCell.vue
@@ -0,0 +1,71 @@
+<template>          
+    <div class="com-cell" :class="{link: type=='link' ? true : false, border: border }">
+        <h5>{{title}}</h5>
+        <a href="javascript:;" @click="clickFunc">
+            <slot name="body">
+                默认内容
+            </slot>
+            <slot name="footer">
+            </slot>
+        </a>
+    </div>
+</template>
+<script>
+    export default {
+        data(){
+            return {
+
+            }
+        },
+        props: {
+            type: {
+                type: String,
+                default: "cell"
+            },
+            title: {
+                type: String,
+            },
+            border: {
+                type: Boolean,
+                default: false
+            }
+        },
+        methods: {
+            clickFunc(){
+                this.$emit("clickHandle")
+            }
+        }
+    }
+</script>
+<style scoped>
+    .com-cell {
+        padding: .5rem 1rem .5rem 0;
+    }
+    .com-cell.border {
+        border-bottom: 1px solid #656565;
+    }
+    .com-cell a{
+        color: #fff;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        position: relative
+    }
+    .com-cell.link a::after {
+        content: "";
+        display: inline-block;
+        width: 1.5rem;
+        height: 1.5rem;
+        background: url(../assets/icon-left-arrow.png) no-repeat;
+        background-size: 100%;
+        position: absolute;
+        right: -1.2rem;
+        margin-top: -.75rem;
+        top: 50%;
+    }
+    h5 {
+        font-size: 1rem;
+        font-weight: normal;
+        margin-bottom: .25rem;
+    }
+</style>
--- src/views/payOrder.vue
+++ src/views/payOrder.vue
@@ -1,70 +1,78 @@
 <template>
     <div class="pay-order">
-        <div class="weui-cells">
-             <router-link class="weui-cell weui-cell_access" to="/userInfo">
-                <div class="weui-cell__bd">
-                    <p>订餐人信息</p>
+        <com-cell type="link" @clickHandle="editUserInfo">
+            <div slot="body">
+                订餐人信息
+            </div>
+            <div slot="footer">
+                <div v-show="userInfoIsNull" class="active">
+                    +添加
                 </div>
-                <div class="weui-cell__ft">
-                    <span v-show="userInfoIsNull" style="color: #ff8400">+添加</span>
+                <div v-show="!userInfoIsNull">
+
                 </div>
-            </router-link>
-        </div>
-        <div class="weui-panel weui-panel_access">
-            <div class="weui-panel__hd">取餐地址</div>
-            <div class="weui-panel__ft">
-                <router-link to="/address" class="weui-cell weui-cell_access weui-cell_link">
-                    <div class="weui-cell__bd">
-                        <span v-show="addressIsNull">选择取餐地址</span>
-                        <input type="text" name="time" value="" v-show="!addressIsNull">
-                    </div>
-                    <span class="weui-cell__ft"></span>
-                </router-link>
+            </div>
+        </com-cell>
+        <com-cell type="link" title="取餐地址" :border="true" @clickHandle="addAddress">
+            <div slot="body">
+                <div v-show="addressIsNull">
+                    选择取餐地址
+                </div>
+                <div v-show="!userInfoIsNull">
+
+                </div>
+            </div>
+        </com-cell>
+        <com-cell type="link" :border="true" >
+            <div slot="body">
+                取餐时间
+            </div>
+            <div slot="footer">
+                <div v-show="timeIsNull" class="active">
+                    选择取餐时间
+                </div>
+                <div v-show="!userInfoIsNull">
+
+                </div>
+            </div>
+        </com-cell>
+        <com-cell :border="true">
+            <div slot="body" class="tip">
+                <span class="icon icon-tip"> </span>我们会尽力保障外卖的送达时间
+            </div>
+        </com-cell>
+
+        <div class="product-list">
+            <com-cell v-for="(item, index) in products" :border="index === products.length - 1 ? true : false">
+                <div slot="body" class="product-item">
+                    <span>{{item.name}}</span>
+                    <span>x{{item.number}}</span>
+                    <span>￥{{item.price}}</span>
+                </div>
+            </com-cell>
+            <div class="total">
+                <span>共{{total}}份, </span>
+                <span>总计：<b>{{price}}</b>元</span>
             </div>
         </div>
-        <div class="weui-cells">
-            <a class="weui-cell weui-cell_access" href="javascript:;">
-                <div class="weui-cell__bd">
-                    <p>取餐时间</p>
-                </div>
-                <div class="weui-cell__ft">
-                    <span v-show="timeIsNull">选择取餐时间</span>
-                </div>
-            </a>
-        </div>
-        <div class="weui-cell">
-            <div class="weui-cell__hd">
-                <img src="../assets/icon-tip.png" alt="" style="width:20px;margin-right:5px;display:block"></div>
-            <div class="weui-cell__bd">
-                <p>会尽力保障便当的送达时间</p>
+        <com-cell :border="true" title="订单备注">
+            <div slot="body">
+                <input type="text" placeholder="输入您的用餐习惯">
             </div>
-        </div>
-        <ul class="list-view">
-            <li v-for="item in products">
-                <span>{{item.name}}</span>
-                <span>x{{item.number}}</span>
-                <span>￥{{item.price}}</span>
-            </li>
-        </ul>
-        <div class="total">
-            <span>共{{total}}份, </span>
-            <span>总计：<b>{{price}}</b>元</span>
-        </div>
-       <weui-panel title="备注信息">
-           <input type="text" name="address" slot="cell-bd" placeholder="请输入您的用餐习惯">
-       </weui-panel>
-       <weui-panel :hideIcon="true" title="支付方式">
-            <p slot="cell-bd">微信支付</p>
-            <span class="icon icon-selected" slot="cell-ft"></span>
-       </weui-panel>
-        <div class="footer">
-          <div class="price">待支付:￥{{price}}</div>
-          <div class="sumit" @click="submitOrder">下单</div>
-        </div>
+        </com-cell>
+        <com-cell :border="true" title="付款方式">
+            <div slot="body">
+                微信支付
+            </div>
+            <div slot="footer" class="pay-way">
+                <span class="icon icon-selected"></span>
+            </div>
+        </com-cell>
     </div>
 </template>
 <script>
-    import weuiPanel from '../components/weuiPanel'
+    import comCell from '../components/comCell'
+
     export default {
         data() {
             return {
@@ -85,104 +93,74 @@
             }
         },
         methods: {
-            submitOrder(){
+            submitOrder() {
                 alert("submit order")
+            },
+            editUserInfo(){
+                this.$router.push({
+                    path: "/userInfo"
+                })
+            },
+            addAddress(){
+                this.$router.push({
+                    path: "/address"
+                })
             }
         },
         components: {
-            weuiPanel
+            comCell
         }
     }
 </script>
 <style scoped>
-   .weui-cell__bd input,
-   .weui-cell__bd,
-   .weui-panel__hd,
-   .weui-cells {
-       font-size: 1rem;
-       outline: none;
-   }
+    .product-list .com-cell:last-child {
+        border-bottom: 1px solid #656565
+    }
+    .active {
+        color: #ff8400;
+    }
+    .tip {
+        color: #999;
+        display: flex;
+    }
+    .pay-way {
+        display: flex;
+        align-items: center
+    }
+     .icon.icon-tip,
+    .icon.icon-selected{
+        width: 1.5rem;
+        height: 1.5rem;
+        display: inline-block;
+    }
+    .icon.icon-tip {
+        background: url(../assets/icon-tip.png);
+        background-size: 100%;
+    }
+    .icon.icon-selected {
+        background: url(../assets/icon-selected.png);
+        background-size: 100%;
+    }
    .pay-order {
        padding: .5rem;
        min-height: calc(100vh - 3rem);
+       background: #1a1b1f;
+       color: #fff;
+   }
+   .product-item {
+       display: flex;
+       width: 100%;
+       justify-content: space-between
    }
    input {
        color: #fff;
        border: none;
-       background: transparent
+       background: transparent;
+       outline: none;
    }
-   .weui-cells,
-   .pay-order,
-  .weui-panel {
-      background: #1a1b1f;
-      color: #fff;
-  }
-  .weui-cells,
-  .weui-panel {
-      margin-top: 0;
-  }
-  .weui-panel:after,
-  .weui-cells:after {
-      left: 15px
-  }
-  .weui-cells:before,
-  .weui-cell:before,
-  .weui-panel:before {
-      display: none;
-  }
- 
-  .weui-cell {
-      padding: 10px 15px
-  }
-  .weui-cell_link {
-      color:#fff;
-  }
-  .icon.icon-selected{
-      width: 1.5rem;
-      height: 1.5rem;
-      display: inline-block;
-      background: url(../assets/icon-selected.png);
-      background-size: 100%;
- }
- .weui-cell_access:active {
-     background: transparent
- }
- .list-view {
-     padding: 10px 0px;
-     list-style-type: none;
-     border-top: 1px solid #656565;
-     border-bottom: 1px solid #656565;
-     margin: 0 0 0 15px;
- }
- .list-view li {
-     display: flex;
-     justify-content: space-between;
-     margin-top: .5rem;
- }
  .total {
      display: flex;
      justify-content: flex-end;
      margin: .3rem .8rem;
  }
- .footer {
-     position: fixed;
-     bottom: 0;
-     background: #272727;
-     color: #fff;
-     width: 100%;
-     left: 0;
-     display: flex;
-     box-sizing: border-box;
- }
- .footer div {
-     padding: .8rem .5rem;
-     text-align: center
- }
-.footer div:first-child {
-    width: 55%;
-}
-.footer div:last-child {
-    width: 45%;
-    background: #ff8400;
-}
 </style>
